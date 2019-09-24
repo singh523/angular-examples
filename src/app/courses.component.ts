da6c1c53364477e4034fbabd9fa3c415d28ac137
Vijay Singh <vijay.singh@qantasloyalty.com>
2019-09-24 22:50:08 +1000

    Bootstrap included

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,29 +3,24 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: `
-        <h2>{{getTitle()}}</h2>
-        <ul>
-            <li *ngFor = "let course of courses">
-                {{ course }}
-            </li>
-        </ul>
+    /*template: `
+        <h2>{{title}}</h2>
+        <img src="{{imageUrl}}"/>
+        <h1 [textContent]="title"></h1>
+        <img [src] = "title"/>
+        <table>
+            <tr>
+                <td [attr.colspan]="colspan"></td>
+            </tr>
+        </table>
     `
+    */
+   template: `
+   <button class="btn btn-primary" [class.active]="isActive">Save</button>
+   `
 })
 export class CoursesComponent {
     title = "List of course";
-    courses;
-
-    constructor(service: CourseService) {
-        this.courses = service.getCourses();
-    }
-
-
-
-    getTitle() {
-        return this.title;
-    }
-    getCourses() {
-        return this.courses;
-    }
+    imageUrl = "http://www.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-landscape-407021107";
+    colspan=2;
 }
